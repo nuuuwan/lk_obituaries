e536afe023f09ca8c1463a57e8f96406ac771f2a
nuuuwan <nuuuwan@gmail.com>
2023-12-20 10:10:13 +0530

    utils_future

---
+++ src/utils_future/WWW.py
@@ -0,0 +1,21 @@
+from functools import cached_property
+
+import requests
+from bs4 import BeautifulSoup
+from utils import Log
+
+log = Log('WWW')
+
+
+class WWW:
+    def __init__(self, url: str):
+        self.url = url
+
+    @cached_property
+    def html(self) -> str:
+        log.debug(f'Crawling {self.url}')
+        return requests.get(self.url).text
+
+    @property
+    def soup(self):
+        return BeautifulSoup(self.html, 'html.parser')
